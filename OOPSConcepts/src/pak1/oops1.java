b5a3dacedba8e194eb2a7729f5048b2760efe985
sivakumar <sivakumar@DESKTOP-PM35DO9>
2018-06-12 14:08:43 +0530

    OOPS Started

--- OOPSConcepts/src/pak1/oops1.java
+++ OOPSConcepts/src/pak1/oops1.java
@@ -1,15 +1,28 @@
 package pak1;
 
 public class oops1 {
-
+	
 	public static void main(String[] args) 
 	{
-		f1();
-	}
-	
-	public static void f1()
-	{
-		
+		oops1 obj = new oops2();
+		obj.f1();
 	}
 
+	public void f1()
+	{
+		System.out.println("Function 1");
+	}
+
+	public static void f1(int i)
+	{
+		System.out.println("Function 2: "+i);
+	}
+	
+	public static class oops2 extends oops1 {
+		
+		public void f1()
+		{
+			System.out.println("Function 3");
+		}
+	}
 }
