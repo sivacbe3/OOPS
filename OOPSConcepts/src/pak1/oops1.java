8046a5eeb57f71b6319a6bfb9d2f4cad3ae87bd1
sivakumar <sivakumar@DESKTOP-PM35DO9>
2018-06-15 13:29:11 +0530

    M1

--- OOPSConcepts/src/pak1/oops1.java
+++ OOPSConcepts/src/pak1/oops1.java
@@ -9,11 +9,14 @@ public class oops1 {
 		oops2 obj1 = (oops2) obj;
 		obj.f1();
 		obj1.f1();
+		
 	}
 
+	@SuppressWarnings("static-access")
 	public void f1()
 	{
 		System.out.println("Function 1");
+		this.f1(getItems());
 	}
 
 	public static void f1(int i)
